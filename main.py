450865aa9b5c09b0c5689a54d0de3404df4ed29d
Okunyev Oleksandr <a_okunev@ukr.net>
2022-05-25 22:58:40 +0200

    HOME_WORK_1

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+print('Hola El Mundo')
+def summa():
+    a = input("Введите первое слово: ")
+    b = input("Введите второе слово: ")
+    c = input("Введите третье слово: ")
 
+    print(a + " " + b + " " + c)
+
+def my_home_work():
+    a = int(input("Введите число А:"))
+    b = int(input("Введите число В:"))
+    c = int(input("Введите число С:"))
+
+    while a < b:
+        print("Грустное сообщение ")
+        a = a + c
+
+    print("Радостное сообщение")
+
+
+my_home_work()
+summa()
